60ea13995f42194bda88d4c1bf7a78330aed726d
Abtolo <102583032+Abtolo@users.noreply.github.com>
2024-10-10 00:23:57 +0200

    Js code
    changed a bit of code and reassigned the classes and wrote js code

--- Script.js
+++ Script.js
@@ -1,22 +1,82 @@
 "use strict";
 
 let finalAnswer = 0;
-let x;
+let calculation = 0;
 
 let displaySum = function (number) {
-  x = number;
-  document.querySelector(`#btn$[x]`).addEventListener("click", function () {
-    document.querySelector(".responseBar").textContent += x;
-  });
+  document.querySelector(".responseBar").textContent += number;
+  calculation += number;
 };
-displaySum(9);
-displaySum(8);
-displaySum(7);
-displaySum(6);
-displaySum(5);
-displaySum(4);
-displaySum(3);
-displaySum(2);
-displaySum(1);
-displaySum(0);
-displaySum(9);
+
+document.querySelector(".deleteBar").addEventListener("click", function () {
+  document.querySelector(".responseBar").textContent = 0;
+});
+
+document.querySelector(".buttons").addEventListener("click", function () {
+  document.querySelector(".responseBar").textContent;
+  console.log("It works");
+});
+
+document.querySelector("#btn9").addEventListener("click", function () {
+  displaySum(9);
+});
+
+document.querySelector("#btn8").addEventListener("click", function () {
+  displaySum(8);
+});
+
+document.querySelector("#btn7").addEventListener("click", function () {
+  displaySum(7);
+});
+
+document.querySelector("#btn6").addEventListener("click", function () {
+  displaySum(6);
+});
+
+document.querySelector("#btn5").addEventListener("click", function () {
+  displaySum(5);
+});
+
+document.querySelector("#btn4").addEventListener("click", function () {
+  displaySum(4);
+});
+
+document.querySelector("#btn3").addEventListener("click", function () {
+  displaySum(3);
+});
+
+document.querySelector("#btn2").addEventListener("click", function () {
+  displaySum(2);
+});
+
+document.querySelector("#btn1").addEventListener("click", function () {
+  displaySum(1);
+});
+
+document.querySelector("#btn0").addEventListener("click", function () {
+  displaySum(0);
+});
+
+document.querySelector("#btnPlus").addEventListener("click", function () {
+  displaySum("+");
+});
+
+document.querySelector("#btnMult").addEventListener("click", function () {
+  displaySum("x");
+});
+
+document.querySelector("#btnDiv").addEventListener("click", function () {
+  displaySum(":");
+});
+
+document.querySelector("#btnMinus").addEventListener("click", function () {
+  displaySum("-");
+});
+
+document.querySelector("#btnPoint").addEventListener("click", function () {
+  displaySum(".");
+});
+
+document.querySelector("#btn=").addEventListener("click", function () {
+  document.querySelector(".responseBar").textContent = calculation;
+});
