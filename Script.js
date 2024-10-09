379c0c7e120be661e42b33348980fa676fe59b3b
Abtolo <102583032+Abtolo@users.noreply.github.com>
2024-10-09 17:20:57 +0200

    mistake

--- Script.js
+++ Script.js
@@ -1,7 +1,22 @@
 "use strict";
 
 let finalAnswer = 0;
+let x;
 
-document.querySelector(".buttons").addEventListener("click", function () {
-  console.log("Hello, does it work");
-});
+let displaySum = function (number) {
+  x = number;
+  document.querySelector(`#btn$[x]`).addEventListener("click", function () {
+    document.querySelector(".responseBar").textContent += x;
+  });
+};
+displaySum(9);
+displaySum(8);
+displaySum(7);
+displaySum(6);
+displaySum(5);
+displaySum(4);
+displaySum(3);
+displaySum(2);
+displaySum(1);
+displaySum(0);
+displaySum(9);
